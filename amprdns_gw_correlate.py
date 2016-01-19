af190f500a7dcb460484b1039d3faeb2d8602d54
Neil Johnson <njohnsn@users.noreply.github.com>
2016-01-18 20:04:13 -0600

    Add GPL Header

--- amprdns_gw_correlate.py
+++ amprdns_gw_correlate.py
@@ -6,6 +6,23 @@ an encap file (encap.txt) and determines whether there is a
 gateway assocated with each AMPRnet (44.0.0.0/8) A record in
 the zone file.
 '''
+'''
+    Copyright (C) 2016  Neil Johnson
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+'''
 
 import ipaddress
 
