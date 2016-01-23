3eccc78d7efe2a34c58b6eb907dcd3eef5862467
Neil Johnson <neil-johnson@uiowa.edu>
2016-01-23 12:28:57 -0600

    Combined portal and gw lookup scripts into one. Output files

--- amprdns_portal_correlate.py
+++ amprdns_portal_correlate.py
@@ -5,8 +5,7 @@ This script reads in the AMPRNet DNS zone file (ampr.org) and
 an encap file (encap.txt) and determines whether there is a 
 gateway assocated with each AMPRnet (44.0.0.0/8) A record in 
 the zone file.
-'''
-'''
+
     Copyright (C) 2016  Neil Johnson 
 
     This program is free software; you can redistribute it and/or modify
@@ -22,17 +21,33 @@ the zone file.
     You should have received a copy of the GNU General Public License along
     with this program; if not, write to the Free Software Foundation, Inc.,
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-
 '''
  
 import ipaddress
 
-subnets={}
+# Constants
+AMPR_NET = ipaddress.ip_network('44.0.0.0/8')
+ENCAP_FILE = 'encap.txt'
+PORTAL_NETS_FILE = 'portal_nets.txt'
+ZONE_FILE = 'ampr.org'
 
-ampr_net = ipaddress.ip_network('44.0.0.0/8')
+# Loop through portal nets file
+subnets=[]
+with open(PORTAL_NETS_FILE, encoding='utf-8') as portal_file:
+    for portal_line in portal_file:
+        portal_line = portal_line.rstrip()
+
+        network = portal_line
+
+        try:
+            subnets.append(ipaddress.ip_network(network))
+        except ValueError as err:
+            print('ERROR: {0}'.format(err))
 
 # Loop through encap file
-with open('encap.txt', encoding='utf-8') as encap_file:
+gw_nets = {}
+
+with open(ENCAP_FILE, encoding='utf-8') as encap_file:
     for encap_line in encap_file:
         encap_line = encap_line.rstrip()
 
@@ -55,12 +70,12 @@ with open('encap.txt', encoding='utf-8') as encap_file:
             else:
                 print('Error network: ', network)
 
-            subnets[ipaddress.ip_network(real_network)] = ipaddress.ip_address(encap_fields[4])
+            gw_nets[ipaddress.ip_network(real_network)] = ipaddress.ip_address(encap_fields[4])
 
-addr_list=[]
 
 # Loop through dns zone file
-with open('ampr.org', encoding='utf-8') as a_file:
+addr_list=[]
+with open(ZONE_FILE, encoding='utf-8') as a_file:
     for a_line in a_file:
         a_line = a_line.rstrip()
 
@@ -73,13 +88,31 @@ with open('ampr.org', encoding='utf-8') as a_file:
                 address = ipaddress.ip_address(fields[3])
                 addr_list.append(address)
 
-# Lookup gateway for address
+# Lookup portal net for address
+addr_to_portal_file = open('address_to_portal_net.txt', 'w') 
 for address in sorted(addr_list):
     # Check if address is in 44.0.0.0/8
-    if (address in ampr_net):
-        for subnet in sorted(subnets.keys()):
+    if (address in AMPR_NET):
+        for subnet in sorted(subnets):
             if ( address in subnet ):
-                print('\"', str(address), '\",\"', str(subnets[subnet]),'\"')
+                addr_to_portal_file.write('\"{0}\",\"{1}\"\n'.format(str(address), str(subnet)))
                 break
         else:
-            print('\"', address, '\",\"GATEWAY_NOT_FOUND\"')
+            addr_to_portal_file.write('\"{0}\",\"{1}\"\n'.format(str(address), 'PORTAL_NET_NOT_FOUND'))
+
+addr_to_portal_file.close()
+
+# Lookup gateway address for address
+addr_to_gw_file = open('address_to_gw.txt', 'w') 
+for address in sorted(addr_list):
+    # Check if address is in 44.0.0.0/8
+    if (address in AMPR_NET):
+        for gw_net in sorted(gw_nets):
+            if ( address in gw_net ):
+                addr_to_gw_file.write('\"{0}\",\"{1}\"\n'.format(str(address), str(gw_nets[gw_net])))
+                break
+        else:
+            addr_to_gw_file.write('\"{0}\",\"{1}\"\n'.format(str(address), 'GATEWAY_NOT_FOUND'))
+
+addr_to_gw_file.close()
+
