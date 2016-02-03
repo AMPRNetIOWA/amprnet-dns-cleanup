f30e4e21bcdd965c56db15f8a49ea96014c21100
Neil Johnson <neil-johnson@uiowa.edu>
2016-02-03 17:11:42 -0600

    New List from Brian

--- amprdns_portal_correlate.py
+++ amprdns_portal_correlate.py
@@ -27,7 +27,6 @@ import ipaddress
 
 # Constants
 AMPR_NET = ipaddress.ip_network('44.0.0.0/8')
-ENCAP_FILE = 'encap.txt'
 PORTAL_NETS_FILE = 'portal_nets.txt'
 ZONE_FILE = 'ampr.org'
 
@@ -44,37 +43,9 @@ with open(PORTAL_NETS_FILE, encoding='utf-8') as portal_file:
         except ValueError as err:
             print('ERROR: {0}'.format(err))
 
-# Loop through encap file
-gw_nets = {}
-
-with open(ENCAP_FILE, encoding='utf-8') as encap_file:
-    for encap_line in encap_file:
-        encap_line = encap_line.rstrip()
-
-        if (encap_line.find('addprivate') != -1): 
-            encap_fields = encap_line.split()
-            network = encap_fields[2]
-
-            octet_list = network.split('.')
-            slash_loc = network.find('/')
-
-            # Add '.0's to subnet entries so they can be parsed by ipaddress module
-            if ( len(octet_list) == 3 ):
-                real_network = network[0:slash_loc] + ".0" + network[slash_loc:]
-            elif ( len(octet_list) == 2 ):
-                real_network = network[0:slash_loc] + ".0.0" + network[slash_loc:]
-            elif ( len(octet_list) == 1 ):
-                real_network = network[0:slash_loc] + ".0.0.0" + network[slash_loc:]
-            elif ( len(octet_list) == 4 ):
-                real_network = network
-            else:
-                print('Error network: ', network)
-
-            gw_nets[ipaddress.ip_network(real_network)] = ipaddress.ip_address(encap_fields[4])
-
+addr_to_portal_file = open('dns_entries_to_delete.txt', 'w') 
 
 # Loop through dns zone file
-addr_list=[]
 with open(ZONE_FILE, encoding='utf-8') as a_file:
     for a_line in a_file:
         a_line = a_line.rstrip()
@@ -86,33 +57,14 @@ with open(ZONE_FILE, encoding='utf-8') as a_file:
             # Pull out 'A' records only 
             if ( fields[2] == 'A' ):
                 address = ipaddress.ip_address(fields[3])
-                addr_list.append(address)
 
-# Lookup portal net for address
-addr_to_portal_file = open('address_to_portal_net.txt', 'w') 
-for address in sorted(addr_list):
-    # Check if address is in 44.0.0.0/8
-    if (address in AMPR_NET):
-        for subnet in sorted(subnets):
-            if ( address in subnet ):
-                addr_to_portal_file.write('\"{0}\",\"{1}\"\n'.format(str(address), str(subnet)))
-                break
-        else:
-            addr_to_portal_file.write('\"{0}\",\"{1}\"\n'.format(str(address), 'PORTAL_NET_NOT_FOUND'))
+                # Check if address is in 44.0.0.0/8
+                if (address in AMPR_NET):
+                    for subnet in sorted(subnets):
+                        if ( address in subnet ):
+                            break
+                    else:
+                        addr_to_portal_file.write('{0}\tDEL\tA\t{1}\n'.format(fields[0], str(address) ))
 
 addr_to_portal_file.close()
-
-# Lookup gateway address for address
-addr_to_gw_file = open('address_to_gw.txt', 'w') 
-for address in sorted(addr_list):
-    # Check if address is in 44.0.0.0/8
-    if (address in AMPR_NET):
-        for gw_net in sorted(gw_nets):
-            if ( address in gw_net ):
-                addr_to_gw_file.write('\"{0}\",\"{1}\"\n'.format(str(address), str(gw_nets[gw_net])))
-                break
-        else:
-            addr_to_gw_file.write('\"{0}\",\"{1}\"\n'.format(str(address), 'GATEWAY_NOT_FOUND'))
-
-addr_to_gw_file.close()
 
